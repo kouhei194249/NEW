fb0f83e4cefbd45e737543ae787a92ecfe1bd46d
kouhei194249 <194249@st.takushoku-u.ac.jp>
2022-02-05 01:08:52 +0900

    08-4

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,4 @@ use Illuminate\Support\Focades\Routes;
 Route::get('/', 'PostController@index');
 Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}', 'PostController@show');
-
 Route::post('/posts', 'PostController@store');
